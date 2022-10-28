16d1a4a465aa2d20a5dbba396c9c8397f5967eb7
hglovercode <helen_glover@outlook.com>
2022-10-28 07:56:39 -0400

    basic navbar, homepage + controllers, login and sign up

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,22 @@
+class UserController < ApplicationController
+
+  skip_before_action :authorized, only: [:new, :create]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(params.require(:user).permit(:username, :password, :name, :email))
+    if @user.save
+      log_in @user
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      #need requirement
+      flash.now[:alert] << "Error: Please check the username password requirements"
+      end
+      render :action => 'new'
+    end
+  end
+end
