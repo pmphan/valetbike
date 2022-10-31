fb82691f58db4b0f364594a4d4b6ccd3b46eea5c
Phuong <pmphan00@gmail.com>
2022-10-31 13:01:23 -0400

    [Feature] Rework registration to use devise.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   validates_presence_of   :first_name,
-                          :last_name,
-                          :email,
-                          :password
-  validates_uniqueness_of :email
-  # TODO: No distinguishing lowercase email and uppercase email
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  # TODO: More secure password requirements
-  has_secure_password
+                          :last_name
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "home#index"
+  devise_for :users
   
   #pls delete once webpage is set
   match 'about', to: 'about#index', via: 'get'
@@ -8,9 +9,7 @@ Rails.application.routes.draw do
 
   #match '/map/:id', to: 'map#show', via: 'get'
   get 'help'    => 'help#index'
-  get 'signup'  => 'users#new'
   get 'home/index'
   get 'login/index'
   get 'rent/index'
-  resources :users
 end
