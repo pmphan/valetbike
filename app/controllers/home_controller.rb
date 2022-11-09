9d0b9e0667013f4d221d2ed943d0f89bfb10a40f
abeltranandrade <abeltranandrade@smith.edu>
2022-11-09 06:20:40 -0500

    We now have custom markers on the map. I also did some changes to the controller to see how I could display data but decided in doing things differently so just left instance variable

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    # created instance variable in order to test the database on homepage
+    @stations = Station.all.order(identifier: :asc)
   end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,7 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-// initializing our map connected to div map in application and css
+//initializing our map connected to div map in application and css
 var map = L.map('map').setView([42.328674, 	-72.664658], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -10,3 +10,49 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+<<<<<<< HEAD
+=======
+
+var hydraIcon = L.icon({
+    iconUrl: '/assets/hydra.png',     
+    iconSize:     [40, 40],   
+    iconAnchor:   [0, 0], 
+    // point of the icon which will correspond to marker's location   
+    popupAnchor:  [20, 0] 
+    // point from which the popup should open relative to the iconAnchor 
+    });
+
+var BikeIcon = L.icon({
+    iconUrl: '/assets/bicycle.svg',     
+    iconSize:     [50, 40],   
+    iconAnchor:   [0, 0], 
+    // point of the icon which will correspond to marker's location   
+    popupAnchor:  [20, 0] 
+    // point from which the popup should open relative to the iconAnchor 
+    });
+
+
+
+var hydraIcon = L.icon({
+    iconUrl: '/assets/hydra.png',     
+    iconSize:     [40, 40],   
+    iconAnchor:   [0, 0], 
+    // point of the icon which will correspond to marker's location   
+    popupAnchor:  [20, 0] 
+    // point from which the popup should open relative to the iconAnchor 
+    });
+
+var BikeIcon = L.icon({
+    iconUrl: '/assets/bicycle.svg',     
+    iconSize:     [50, 40],   
+    iconAnchor:   [0, 0], 
+    // point of the icon which will correspond to marker's location   
+    popupAnchor:  [20, 0] 
+    // point from which the popup should open relative to the iconAnchor 
+    });
+
+
+
+var hydra = L.marker([42.328674, -72.664658], {icon: BikeIcon}).addTo(map);
+var BikingIcon = L.marker([42.428674, -72.664658], {icon: BikeIcon}).addTo(map);
+hydra.bindPopup("<b>Station:</b><br><b>Available Bikes:</b> <br> <b>Start</b><button type=\"button\">Click Me!</button>").openPopup();
