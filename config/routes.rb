26259072e38ffa82aeba2c41f75379eb0b5f6236
Helen-Glover <hglover@smith.edu>
2022-12-07 21:01:37 -0500

    feat: User profile + html/css styling.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "home#index"
   devise_for :users, module: "users"
   resources :stations
+  resources :users
 
   #pls delete once webpage is set
   match 'about', to: 'about#index', via: 'get'
@@ -14,4 +15,5 @@ Rails.application.routes.draw do
   get 'memberships'  => 'memberships#index'
 
   get 'billing' => 'billing#show'
+
 end
